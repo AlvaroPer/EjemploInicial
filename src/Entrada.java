9c94f998a13f35d6e1bd549abc7f357a6082f638
AlvaroPer <55192065+AlvaroPer@users.noreply.github.com>
2019-09-11 18:10:22 +0200

    Update Entrada.java

--- src/Entrada.java
+++ src/Entrada.java
@@ -2,5 +2,7 @@ public class Entrada {
     public static void main(String[] args) {
         System.out.printf("Primer proyecto GitHUb");
         System.out.printf("Segunda linea de codigo");
+        
+        //Modificacion realizada desde la web
     }
 }
